5463f307f408099a8e791ab4e6694f75d17470ea
carmbruster <carmbruster@clearent.com>
2015-09-07 11:53:05 -0500

    Adding pieces for hello world app

---
+++ src/test/java/com/globalhackv/app/GlobalHackVApplicationTests.java
@@ -0,0 +1,25 @@
+package com.globalhackv.app;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GlobalHackVApplication.class)
+@WebAppConfiguration
+public class GlobalHackVApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		GlobalHackVApplication globalHackVApplication = mock(GlobalHackVApplication.class);
+	    String[] args = {""};
+		globalHackVApplication.main(args);
+	    assertTrue(true);	    
+	}
+
+}
