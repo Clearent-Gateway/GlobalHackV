7b8e9569f637b5ffba4ddb1644866095b7291832
carmbruster <carmbruster@clearent.com>
2015-09-12 10:51:02 -0500

    yeah, I'm checking in code baby

---
+++ src/main/resources/frontend/citation/citationListCtrl.js
@@ -0,0 +1,11 @@
+angular
+    .module('court')
+    .controller('CitationListCtrl', ['$scope','$modal', '$location',
+        function($scope,$modal,$location) {
+
+            var vm = this;
+
+
+
+
+        }]);
