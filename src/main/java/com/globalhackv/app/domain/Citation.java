a7b319c4b31a064318e569a8568c4840bc596782
hwills <hunterwills@Hunters-MacBook-Pro.local>
2015-09-11 23:08:58 -0500

    Created citation object

---
+++ src/main/java/com/globalhackv/app/domain/Citation.java
@@ -0,0 +1,116 @@
+
+
+public class Citation {
+
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String driversLiscense;
+    private String address;
+    private String city;
+    private String state;
+    private long citationNumber;
+    private String municapality;
+    private String citationDate;
+    private String courtAddress;
+    private String courtDate;
+
+    public Citation() {
+        
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDriversLiscense() {
+        return driversLiscense;
+    }
+
+    public void setDriversLiscense(String driversLiscense) {
+        this.driversLiscense = driversLiscense;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getCitationNumber() {
+        return citationNumber;
+    }
+
+    public void setCitationNumber(long citationNumber) {
+        this.citationNumber = citationNumber;
+    }
+
+    public String getMunicapality() {
+        return municapality;
+    }
+
+    public void setMunicapality(String municapality) {
+        this.municapality = municapality;
+    }
+
+    public String getCitationDate() {
+        return citationDate;
+    }
+
+    public void setCitationDate(String citationDate) {
+        this.citationDate = citationDate;
+    }
+
+    public String getCourtAddress() {
+        return courtAddress;
+    }
+
+    public void setCourtAddress(String courtAddress) {
+        this.courtAddress = courtAddress;
+    }
+
+    public String getCourtDate() {
+        return courtDate;
+    }
+
+    public void setCourtDate(String courtDate) {
+        this.courtDate = courtDate;
+    }
+}
