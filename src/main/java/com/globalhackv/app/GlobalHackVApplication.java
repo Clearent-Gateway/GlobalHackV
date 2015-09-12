e73bd2726e41be376544c3d3d30a1f5f932d9496
carmbruster <carmbruster@clearent.com>
2015-09-11 19:53:36 -0500

    Removed duplicate httpclient dependency.

---
+++ src/main/java/com/globalhackv/app/GlobalHackVApplication.java
@@ -0,0 +1,12 @@
+package com.globalhackv.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalHackVApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalHackVApplication.class, args);
+    }
+}
