ab2ad8b239a317b4b088c53fe7217904f8caa123
dhigginbotham <dhigginbotham@clearent.com>
2015-09-12 08:33:42 -0500

    Captured a discussion Team SMS had by stubbing out some methods and adding some TODO breadcrumbs.

---
+++ src/main/java/com/globalhackv/app/domain/SMSResponse.java
@@ -0,0 +1,27 @@
+package com.globalhackv.app.domain;
+
+public class SMSResponse {
+
+    private String citationNumber;
+    private String message;
+    
+    public SMSResponse() {
+    	
+    }
+
+	public String getCitationNumber() {
+		return citationNumber;
+	}
+
+	public void setCitationNumber(String citationNumber) {
+		this.citationNumber = citationNumber;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ src/main/java/com/globalhackv/app/repository/SMSRequestRepository.java
@@ -0,0 +1,8 @@
+package com.globalhackv.app.repository;
+
+
+public interface SMSRequestRepository {//extends JpaRepository<SMSRequest,Long>, JpaSpecificationExecutor {
+
+    //List<SMSRequest> findByPhoneNumber(String phoneNumber);
+
+}
