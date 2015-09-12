8f6ffa99661bae4222a98909b8bf6136c667e931
carmbruster <carmbruster@clearent.com>
2015-09-12 00:05:57 -0500

    Adding dummy files and project structure

---
+++ src/main/resources/frontend/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+angular
+    .module('court', [
+        'ngRoute',
+        'ui.bootstrap',
+        'angularMoment',
+        'ngCookies',
+        'infinite-scroll',
+        'angulartics',
+        'angulartics.google.analytics'
+    ]);
