459d59f0153071f46452a73984e6d94205620303
dhigginbotham <dhigginbotham@clearent.com>
2015-09-12 20:50:55 -0500

    sms added more info to the sms response

--- src/main/java/com/globalhackv/app/controllers/SMSController.java
+++ src/main/java/com/globalhackv/app/controllers/SMSController.java
@@ -5,9 +5,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-  
-
-
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.globalhackv.app.domain.Citation;
 import com.globalhackv.app.domain.SMSResponse;
+import com.globalhackv.app.domain.Violation;
+import com.globalhackv.app.repository.ViolationRepository;
 import com.globalhackv.app.service.SMSRequestService;
 
 import javax.servlet.http.HttpServlet;
@@ -24,9 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.HashMap;
- 
-
-
 
 import com.twilio.sdk.verbs.TwiMLResponse;
 import com.twilio.sdk.verbs.TwiMLException;
@@ -38,92 +34,91 @@ public class SMSController {
 	@Autowired
 	SMSRequestService smsRequestService;
 
+	@Autowired
+	ViolationRepository violationRepository;
+
 	@RequestMapping("/sms")
 	public void search(HttpServletRequest request, HttpServletResponse response) {
 		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
 
-        String fromNumber = request.getParameter("From");
-        String body = request.getParameter("Body");
-        System.out.println("body is " + body );
-        System.out.println("fromNumber is " + fromNumber );
-        
-        String returnMessage = "";
-        if("?".equalsIgnoreCase(body)) {
-        	returnMessage = "Welcome to the Ticket Retrieval Hotline. Please enter your first name, last name, and date of birth (mm/dd/yyyy), each separated by a space";
-        } else {
-        	String[] theStrings = body.split(" ");
-        	List<String> searchStrings = new ArrayList<String>();
-        	searchStrings.add(theStrings[0]);
-        	searchStrings.add(theStrings[1]);
-        	searchStrings.add(theStrings[2]);
-    		// TODO Get list of search strings from the request.
-    		//List<String> searchStrings = getSearchStrings(request);
+		String fromNumber = request.getParameter("From");
+		String body = request.getParameter("Body");
+		System.out.println("body is " + body);
+		System.out.println("fromNumber is " + fromNumber);
 
-    		List<Citation> citations = smsRequestService.searchForCitations(fromNumber, searchStrings);
+		String returnMessage = "";
+		if ("?".equalsIgnoreCase(body)) {
+			returnMessage = "Welcome to the Ticket Retrieval Hotline. Please enter either your driver's license number or your first name, last name, and date of birth (mm/dd/yyyy); each separated by a space";
+		} else {
+			String[] theStrings = body.split(" ");
+			List<String> searchStrings = new ArrayList<String>();
+			searchStrings.add(theStrings[0]);
+			searchStrings.add(theStrings[1]);
+			searchStrings.add(theStrings[2]);
+			// TODO Get list of search strings from the request.
+			// List<String> searchStrings = getSearchStrings(request);
 
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			List<Citation> citations = smsRequestService.searchForCitations(
+					fromNumber, searchStrings);
 
-            SimpleDateFormat newFormat = new SimpleDateFormat("MM/dd/yyyy");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
-            if(!citations.isEmpty()) {
-    			StringBuilder sb = new StringBuilder();
+			SimpleDateFormat newFormat = new SimpleDateFormat("MM/dd/yyyy");
+			if (!citations.isEmpty()) {
+				StringBuilder sb = new StringBuilder();
+				try {
 
-                    try {
+					for (Citation citation : citations) {
+						Date date = sdf.parse(citation.getCitationDate());
+						String stringDate = newFormat.format(date);
+						Date date2 = sdf.parse(citation.getCourtDate());
+						String stringCourtDate = newFormat.format(date2);
+						String violation = "";
+						String warrant = "no";
+						List<Violation> violations = violationRepository.findByCitationNumber(citation.getCitationNumber());
+						if(!violations.isEmpty()) {
+							violation = violations.get(0).getViolationDescription();
+							if(violations.get(0).isWarrantStatus()) {
+								warrant = "yes";
+							}
+						}
+						
+						sb.append(" Ticket #: " + citation.getCitationNumber()
+								+ " Ticket Date: " + stringDate + " Court Address: "
+								+ citation.getCourtAddress() + " Court Date:"
+								+ stringCourtDate + " Violation: " + violation + " Warrant? " + warrant);
+					}
+					returnMessage = sb.toString();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			} else {
+				returnMessage = "No tickets found";
+			}
 
-                        for(Citation citation: citations) {
-                        Date date = sdf.parse(citation.getCitationDate());
-                        String stringDate = newFormat.format(date);
+		}
+		
+        System.out.println("sms response is " + returnMessage);
+		updateResponse(response, returnMessage);
 
-
-                        sb.append(" Ticket #: " + citation.getCitationNumber() + " Date: " +stringDate );
-                    }
-                    returnMessage = sb.toString();
-                    }catch (Exception e){
-                        System.out.println(e);
-                    }
-
-
-    		} else {
-    			returnMessage = "No tickets found";    			
-    		}
-    		
-        }
-        
-        updateResponse(response, returnMessage);
-        	
 	}
-	
+
 	private void updateResponse(HttpServletResponse response, String message) {
 
-        // Create a TwiML response and add our friendly message.
-        TwiMLResponse twiml = new TwiMLResponse();
-        Message sms = new Message(message);
-        try {
-            twiml.append(sms);
-        } catch (TwiMLException e) {
-            e.printStackTrace();
-        }
- 
-        response.setContentType("application/xml");
-        try {
-			response.getWriter().print(twiml.toXML());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		// Create a TwiML response and add our friendly message.
+		TwiMLResponse twiml = new TwiMLResponse();
+		Message sms = new Message(message);
+		try {
+			twiml.append(sms);
+		} catch (TwiMLException e) {
 			e.printStackTrace();
 		}
-	}
 
-	// TODO can we identify what each string represents ?
-	private List<String> getSearchStrings(HttpServletRequest request) {
-		List<String> searchStrings = new ArrayList<String>();
-		String firstName = "Peter";
-		String lastName = "Sherman";
-		searchStrings.add(firstName);
-		searchStrings.add(lastName);
-		return searchStrings;
-	}
-
-	private String getPhoneNumber(HttpServletRequest request) {
-		return "3148675309";
+		response.setContentType("application/xml");
+		try {
+			response.getWriter().print(twiml.toXML());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
